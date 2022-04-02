3f2d14c18e185af6c2c9039b99d57d4d1e6dd936
ilya-z <ilya-z@auto.kz>
2022-04-02 15:13:12 +0600

    Fix:init remove action in proto service order:order

--- order/order/internal/server/methods.go
+++ order/order/internal/server/methods.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Yujiman/e_commerce/goods/order/order/internal/handler/add"
 	"github.com/Yujiman/e_commerce/goods/order/order/internal/handler/find"
 	"github.com/Yujiman/e_commerce/goods/order/order/internal/handler/getAll"
+	"github.com/Yujiman/e_commerce/goods/order/order/internal/handler/remove"
 	"github.com/Yujiman/e_commerce/goods/order/order/internal/handler/update"
 	pb "github.com/Yujiman/e_commerce/goods/order/order/internal/proto/order"
 )
@@ -21,4 +22,8 @@ func (Server) Find(ctx context.Context, req *pb.FindRequest) (*pb.Orders, error)
 }
 func (Server) Update(ctx context.Context, req *pb.UpdateRequest) (*pb.UUID, error) {
 	return update.Handle(ctx, req)
+}
+
+func (Server) Remove(ctx context.Context, req *pb.RemoveRequest) (*pb.UUID, error) {
+	return remove.Handle(ctx, req)
 }
